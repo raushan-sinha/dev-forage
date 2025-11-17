4f85f325bdef4b25039af664b6477f3a34dc9500
Raushan Sinha <raushankrsinha2004@gmail.com>
2025-11-17 19:27:12 +0530

    Created file for Scroll to top

---
+++ src/components/ScrollToTop.jsx
@@ -0,0 +1,12 @@
+import React, { useEffect } from 'react'
+import { useLocation } from 'react-router-dom'
+
+export default function ScrollToTop() {
+    const path = useLocation();
+
+    useEffect(() => {
+        window.scrollTo(0, 0);
+    }, [path]);
+
+    return null;
+}
