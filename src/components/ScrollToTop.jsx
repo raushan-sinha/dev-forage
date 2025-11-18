cea8293166c3caecb89e76efe8f11253249ec25e
Raushan Sinha <raushankrsinha2004@gmail.com>
2025-11-18 19:10:28 +0530

    code updated with 'smooth' scrolling

--- src/components/ScrollToTop.jsx
+++ src/components/ScrollToTop.jsx
@@ -5,7 +5,11 @@ export default function ScrollToTop() {
     const path = useLocation();
 
     useEffect(() => {
-        window.scrollTo(0, 0);
+        window.scrollTo({
+            top: 0,
+            left: 0,
+            behavior: 'smooth'
+        });
     }, [path]);
 
     return null;
