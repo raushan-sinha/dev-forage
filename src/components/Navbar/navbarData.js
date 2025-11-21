922fd45d54ad34fb48954710281e84fe6f26bfa2
Raushan Sinha <raushankrsinha2004@gmail.com>
2025-11-21 20:22:44 +0530

    Link changed with 'Roadmap'

--- src/components/Navbar/navbarData.js
+++ src/components/Navbar/navbarData.js
@@ -4,7 +4,7 @@ export const navbarData = {
         { id: 1, name: "Home", href: "/" },
         { id: 2, name: "DevStores", href: "/devStores" },
         { id: 3, name: "Components", href: "/components" },
-        { id: 4, name: "DevChat", href: "/devChat" },
+        { id: 4, name: "Roadmap", href: "/roadmap" },
         { id: 5, name: "Pricing", href: "/pricing" },
     ]
 }
