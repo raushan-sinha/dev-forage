c39c595bdeb793a47dfdde84aef3aeea9fe4eaaf
Raushan Sinha <raushankrsinha2004@gmail.com>
2025-12-04 19:52:41 +0530

    Created this file for export all section files

---
+++ src/pages/Home/sections/index.js
@@ -0,0 +1,6 @@
+export { default as WelcomeSection } from './WelcomeSection';
+export { default as OfferSection } from './OfferSection';
+export { default as RoadmapSection } from './RoadmapSection';
+export { default as DsaSection } from './DsaSection';
+export { default as WhySection } from './WhySection';
+export { default as FutureSection } from './FutureSection';
