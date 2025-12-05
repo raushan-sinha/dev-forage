c6db3f69de309bb233872a014f69630f1d84a985
Raushan Sinha <raushankrsinha2004@gmail.com>
2025-12-05 21:07:41 +0530

    Added input field with responsive code & updated header code

--- src/pages/Roadmap/Roadmap.jsx
+++ src/pages/Roadmap/Roadmap.jsx
@@ -7,14 +7,20 @@ const Roadmap = () => {
         <div className="min-h-screen bg-[#0f172a] text-white px-6 pt-24 pb-16">
             <div className="max-w-7xl mx-auto">
 
-                {/* Header */}
-                <h1 className="text-4xl md:text-5xl font-bold text-center mb-3">
-                    Career Roadmaps
-                </h1>
-
-                <p className="text-center text-gray-300 text-lg max-w-2xl mx-auto mb-12">
-                    Explore industry-aligned learning tracks engineered for high-impact tech careers.
-                </p>
+                {/* Input field for searching Roadmap */}
+                <header className='flex flex-col items-center justify-between gap-3 md:flex-row'>
+                    <div className='flex flex-col items-center gap-3'>
+                        <h1 className="text-4xl md:text-5xl font-bold text-center">
+                            Career Roadmaps
+                        </h1>
+                        <p className="text-center text-gray-300 text-lg max-w-2xl mx-auto">
+                            Explore industry-aligned learning tracks engineered for high-impact tech careers.
+                        </p>
+                    </div>
+                    <div>
+                        <input type="text" placeholder='Search for anything...' className='border border-2 rounded-xl border-cyan-600 w-90 md:w-100 py-4 p-4 md:py-3 text-base md:text-lg text-white-800 font-mono placeholder-slate-400 bg-transparent outline-none' />
+                    </div>
+                </header>
 
                 {/* Cards Grid */}
                 <div className="grid sm:grid-cols-2 lg:grid-cols-3 gap-8 mt-10">
