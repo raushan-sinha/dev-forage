c9219a8cb60290352bcf629b1c862d7d487c86c1
Raushan Sinha <raushankrsinha2004@gmail.com>
2025-12-07 20:52:05 +0530

    updated code with new code

--- src/pages/Roadmap/Roadmap.jsx
+++ src/pages/Roadmap/Roadmap.jsx
@@ -6,19 +6,29 @@ const Roadmap = () => {
     return (
         <div className="min-h-screen bg-[#0f172a] text-white px-6 pt-24 pb-16">
             <div className="max-w-7xl mx-auto">
-
                 {/* Input field for searching Roadmap */}
-                <header className='flex flex-col items-center justify-between gap-3 md:flex-row'>
+                <header className='flex flex-col items-center gap-3 md:justify-between md:flex-row'>
                     <div className='flex flex-col items-center gap-3'>
                         <h1 className="text-4xl md:text-5xl font-bold text-center">
                             Career Roadmaps
                         </h1>
-                        <p className="text-center text-gray-300 text-lg max-w-2xl mx-auto">
+                        <p className="text-center text-gray-300 text-lg max-w-2xl mx-auto lg:whitespace-nowrap">
                             Explore industry-aligned learning tracks engineered for high-impact tech careers.
                         </p>
                     </div>
-                    <div>
-                        <input type="text" placeholder='Search for anything...' className='border-2 rounded-xl border-cyan-600 w-90 md:w-100 py-4 p-4 md:py-3 text-base md:text-lg text-white-800 font-mono placeholder-slate-400 bg-transparent outline-none' />
+                    <div className="w-full max-w-sm sm:max-w-md md:max-w-lg lg:max-w-xl xl:max-w-2xl mx-auto relative" id="input">
+                        <input
+                            placeholder="eg. AI Engineer"
+                            id="floating_outlined"
+                            type="text"
+                            className="block w-full text-sm h-[50px] px-4 text-slate-900 bg-white rounded-[8px] border border-slate-200 focus:border-primary focus:outline-primary focus:ring-0 hover:border-brand-500-secondary overflow-ellipsis overflow-hidden text-nowrap pr-[48px]"
+                        />
+                        <div className="absolute top-3 right-3">
+                            <svg fill="slate-300" viewBox="0 0 24 24" height={24} width={24} className='cursor-pointer hover:opacity-80 transition'>
+                                <path d="M10.979 16.8991C11.0591 17.4633 10.6657 17.9926 10.0959 17.9994C8.52021 18.0183 6.96549 17.5712 5.63246 16.7026C4.00976 15.6452 2.82575 14.035 2.30018 12.1709C1.77461 10.3068 1.94315 8.31525 2.77453 6.56596C3.60592 4.81667 5.04368 3.42838 6.82101 2.65875C8.59833 1.88911 10.5945 1.79039 12.4391 2.3809C14.2837 2.97141 15.8514 4.21105 16.8514 5.86977C17.8513 7.52849 18.2155 9.49365 17.8764 11.4005C17.5979 12.967 16.8603 14.4068 15.7684 15.543C15.3736 15.9539 14.7184 15.8787 14.3617 15.4343C14.0051 14.9899 14.0846 14.3455 14.4606 13.9173C15.1719 13.1073 15.6538 12.1134 15.8448 11.0393C16.0964 9.62426 15.8261 8.166 15.0841 6.93513C14.3421 5.70426 13.1788 4.78438 11.81 4.34618C10.4412 3.90799 8.95988 3.98125 7.641 4.55236C6.32213 5.12348 5.25522 6.15367 4.63828 7.45174C4.02135 8.74982 3.89628 10.2276 4.28629 11.6109C4.67629 12.9942 5.55489 14.1891 6.75903 14.9737C7.67308 15.5693 8.72759 15.8979 9.80504 15.9333C10.3746 15.952 10.8989 16.3349 10.979 16.8991Z" />
+                                <rect transform="rotate(-49.6812 12.2469 14.8859)" rx={1} height="10.1881" width={2} y="14.8859" x="12.2469" />
+                            </svg>
+                        </div>
                     </div>
                 </header>
 
@@ -30,8 +40,8 @@ const Roadmap = () => {
                             {...track} />
                     ))}
                 </div>
-            </div>
-        </div>
+            </div >
+        </div >
     );
 }
 
