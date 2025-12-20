10a0856417a6b3b2ffab6bfa39f6e4db6d2da75e
Raushan Sinha <raushankrsinha2004@gmail.com>
2025-12-20 12:09:52 +0530

    Added logics for handle Google & LinkedIn account

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,7 +1,21 @@
-import React from 'react';
+import { useEffect } from 'react';
 import { OfferSection, RoadmapSection, WelcomeSection, DsaSection, WhySection, FutureSection } from './sections';
+import { getLoggedInUser } from '../../services/authService';
 
 export default function Home() {
+    useEffect(() => {
+        const fetchUser = async () => {
+            try {
+                const data = await getLoggedInUser();
+                localStorage.setItem("user", JSON.stringify(data.user));
+            } catch (err) {
+                console.log("Not logged in", err);
+            }
+        };
+
+        fetchUser();
+    }, []);
+
     return (
         <>
             <WelcomeSection />
