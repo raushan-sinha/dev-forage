227546c52ef363781f90fad524616f31aad1c9a8
Raushan Sinha <raushankrsinha2004@gmail.com>
2025-12-20 12:08:30 +0530

    added a logic for login user

--- src/services/authService.js
+++ src/services/authService.js
@@ -9,3 +9,17 @@ export async function signUpUser(credentials) {
     const res = await api.post("/api/auth/register", credentials);
     return res.data;
 };
+
+export const getLoggedInUser = async () => {
+    const res = await fetch(
+        "http://localhost:5000/api/social-auth/get-user",
+        {
+            method: "GET",
+            credentials: "include",
+        }
+    );
+
+    if (!res.ok) throw new Error("Unauthorized");
+
+    return res.json();
+};
