e19a08063159ab079b03e2cb2b1ebfba6a8fc000
Raushan Sinha <raushankrsinha2004@gmail.com>
2026-01-21 20:10:31 +0530

    Code updated with - main, section and padding

--- src/pages/RefundPolicy/Refund.jsx
+++ src/pages/RefundPolicy/Refund.jsx
@@ -11,13 +11,13 @@ import {
 
 export default function Refund() {
     return (
-        <div className="min-h-screen w-full bg-gradient-to-br from-[#0f172a] via-[#1e293b] to-[#0f172a] text-white flex flex-col items-center px-4 py-20">
+        <main className="min-h-screen w-full bg-gradient-to-br from-[#0f172a] via-[#1e293b] to-[#0f172a] text-white flex flex-col items-center px-4 py-25">
             <div className="w-full max-w-4xl bg-white/10 backdrop-blur-lg shadow-xl rounded-2xl p-8 border border-white/10">
                 <h1 className="text-3xl whitespace-nowrap md:text-4xl font-extrabold text-center text-cyan-400 mb-8">
                     Refund & Policy
                 </h1>
 
-                <div className="space-y-6 text-gray-200 text-sm md:text-base leading-relaxed">
+                <section className="space-y-6 text-gray-200 text-sm md:text-base leading-relaxed">
                     <Overview id={1} />
 
                     <Eligibility id={2} />
@@ -31,8 +31,8 @@ export default function Refund() {
                     <RefundRequest id={6} />
 
                     <PolicyUpdates id={7} />
-                </div>
+                </section>
             </div>
-        </div>
+        </main>
     );
 };
