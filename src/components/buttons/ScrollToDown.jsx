5ae7866d508fdcf373a603c1541fb342afb839f8
Raushan Sinha <raushankrsinha2004@gmail.com>
2026-01-29 11:08:05 +0530

    Added a logic for SCroll down and up

--- src/components/buttons/ScrollToDown.jsx
+++ src/components/buttons/ScrollToDown.jsx
@@ -1,7 +1,12 @@
-import { ArrowDownward } from '@mui/icons-material'
-import React from 'react'
+import { ArrowDownward, ArrowUpward } from '@mui/icons-material'
+import { useEffect, useState } from 'react';
+import { useLocation } from 'react-router-dom';
 
 const ScrollToDown = () => {
+    const [scroll, setScroll] = useState(false);
+    const location = useLocation();
+
+    //TODO: Direct render to down section -
     const scrollDown = () => {
         window.scrollTo({
             top: document.body.scrollHeight,
@@ -9,10 +14,37 @@ const ScrollToDown = () => {
         });
     }
 
+    //TODO: Direct render to down section -
+    const scrollTop = () => {
+        window.scrollTo({
+            top: 0,
+            left: 0,
+            behavior: 'smooth'
+        });
+    }
+
+    //TODO: Scroll reset after changing the page -
+    useEffect(() => {
+        setScroll(false);
+    }, [location.pathname]);
+
+
     return (
-        <button className='bg-blue-500 fixed bottom-3 right-5 px-3 py-1 rounded-xl cursor-pointer' onClick={scrollDown}>
-            <ArrowDownward fontSize='medium' />
-        </button>
+        <>
+            <div onClick={() => setScroll(!scroll)}>
+                {scroll ? (
+                    <button className='bg-red-400 fixed bottom-3 right-5 px-3 py-1 rounded-t-full cursor-pointer' onClick={scrollTop}>
+                        <ArrowUpward fontSize='medium' />
+                    </button>
+                ) : (
+                    <button className='bg-blue-500 fixed bottom-3 right-5 px-3 py-1 rounded-b-full cursor-pointer' onClick={scrollDown}>
+                        <ArrowDownward fontSize='medium' />
+                    </button>
+                )}
+            </div>
+        </>
+
+
     )
 }
 
