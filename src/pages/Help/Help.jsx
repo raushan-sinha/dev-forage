22dcb34afd3cafd6fe4e4777e1e4e9a7c5805a6f
Raushan Sinha <raushankrsinha2004@gmail.com>
2026-01-30 14:50:01 +0530

    Input focus on reload

--- src/pages/Help/Help.jsx
+++ src/pages/Help/Help.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useEffect, useRef, useState } from 'react'
 import { InputField, TextareaField, FileUpload, SelectField, SubmitBtn } from '../../components/FormField';
 import Alert from '@mui/material/Alert';
 
@@ -29,6 +29,12 @@ export default function Help() {
     const [name, setName] = useState('');
     const [error, setError] = useState('');
     const [email, setEmail] = useState('');
+    const inputRef = useRef(null);
+
+    //TODO: Input focus on reload -
+    useEffect(() => {
+        inputRef.current.focus();
+    }, []);
 
     //todo: Form Validation -
     const handleSubmit = (event) => {
@@ -43,133 +49,134 @@ export default function Help() {
     }
 
     return (
-            <div className="min-h-screen flex flex-col items-center justify-center bg-gradient-to-br from-[#0f172a] via-[#1e293b] to-[#0f172a] px-4 py-10">
-                <div className="max-w-2xl text-center mt-10 py-3">
-                    <h1 className="text-3xl md:text-4xl font-bold text-white mb-3">Help Desk</h1>
-                    <p className="text-gray-300 text-base md:text-lg">
-                        Need assistance? Fill out the form below and our support team will get back to you within 24–48 hours.
-                    </p>
-                </div>
-                <form className="w-full max-w-2xl bg-[#1e293b]/80 backdrop-blur-lg p-6 md:p-10 rounded-2xl shadow-[0_0px_15px_rgba(56,189,248,0.3)] space-y-5 text-white" onSubmit={handleSubmit}>
-                    <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
-                        <div>
-                            <InputField
-                                label='Full Name'
-                                id='name'
-                                type='text'
-                                name='name'
-                                autoComplete='username'
-                                placeholder="Enter your full name"
-                                value={name}
-                                onChange={(e) => setName(e.target.value)}
-                            />
-                        </div>
-                        <div>
-                            <InputField
-                                label='Email'
-                                id='email'
-                                type='email'
-                                name='email'
-                                autoComplete='email'
-                                placeholder="Enter your email"
-                                value={email}
-                                onChange={(e) => setEmail(e.target.value)}
-                            />
-                        </div>
-                    </div>
-
-                    <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
-                        <div>
-                            <SelectField
-                                label='User Role'
-                                id='role'
-                                autoComplete='role'
-                                options={userRoleOpts}
-                            />
-                        </div>
-                        <div>
-                            <InputField
-                                label='User ID (optional)'
-                                id='userId'
-                                type='text'
-                                name='userId'
-                                autoComplete='userId'
-                                placeholder="User ID"
-                            />
-                        </div>
-                    </div>
-
+        <div className="min-h-screen flex flex-col items-center justify-center bg-gradient-to-br from-[#0f172a] via-[#1e293b] to-[#0f172a] px-4 py-10">
+            <div className="max-w-2xl text-center mt-10 py-3">
+                <h1 className="text-3xl md:text-4xl font-bold text-white mb-3">Help Desk</h1>
+                <p className="text-gray-300 text-base md:text-lg">
+                    Need assistance? Fill out the form below and our support team will get back to you within 24–48 hours.
+                </p>
+            </div>
+            <form className="w-full max-w-2xl bg-[#1e293b]/80 backdrop-blur-lg p-6 md:p-10 rounded-2xl shadow-[0_0px_15px_rgba(56,189,248,0.3)] space-y-5 text-white" onSubmit={handleSubmit}>
+                <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
                     <div>
-                        <SelectField
-                            label='Issue Category'
-                            id='category'
-                            autoComplete='category'
-                            options={issueCategoryOpts}
+                        <InputField
+                            label='Full Name'
+                            id='name'
+                            type='text'
+                            name='name'
+                            autoComplete='username'
+                            placeholder="Enter your full name"
+                            value={name}
+                            ref={inputRef}
+                            onChange={(e) => setName(e.target.value)}
                         />
                     </div>
                     <div>
                         <InputField
-                            label='Subject'
-                            id='subject'
+                            label='Email'
+                            id='email'
+                            type='email'
+                            name='email'
+                            autoComplete='email'
+                            placeholder="Enter your email"
+                            value={email}
+                            onChange={(e) => setEmail(e.target.value)}
+                        />
+                    </div>
+                </div>
+
+                <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
+                    <div>
+                        <SelectField
+                            label='User Role'
+                            id='role'
+                            autoComplete='role'
+                            options={userRoleOpts}
+                        />
+                    </div>
+                    <div>
+                        <InputField
+                            label='User ID (optional)'
+                            id='userId'
                             type='text'
-                            name='subject'
-                            autoComplete='subject'
-                            placeholder="Write a short title for your issue"
+                            name='userId'
+                            autoComplete='userId'
+                            placeholder="User ID"
                         />
                     </div>
+                </div>
 
+                <div>
+                    <SelectField
+                        label='Issue Category'
+                        id='category'
+                        autoComplete='category'
+                        options={issueCategoryOpts}
+                    />
+                </div>
+                <div>
+                    <InputField
+                        label='Subject'
+                        id='subject'
+                        type='text'
+                        name='subject'
+                        autoComplete='subject'
+                        placeholder="Write a short title for your issue"
+                    />
+                </div>
+
+                <div>
+                    <TextareaField
+                        label='Describe Your Issue'
+                        rows="4"
+                        id='description'
+                        autoComplete='description'
+                        placeholder='eg. "My Course isn`t loading." or "How to reset password."'
+                    />
+                </div>
+
+                <div>
+                    <FileUpload
+                        label='Choose File'
+                        type="file"
+                        id='attachFile'
+                        autoComplete='attachFile'
+                    />
+                </div>
+
+                <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
                     <div>
-                        <TextareaField
-                            label='Describe Your Issue'
-                            rows="4"
-                            id='description'
-                            autoComplete='description'
-                            placeholder='eg. "My Course isn`t loading." or "How to reset password."'
+                        <SelectField
+                            label='Priority'
+                            id='priority'
+                            autoComplete='priority'
+                            options={priorityOpts}
                         />
                     </div>
-
                     <div>
-                        <FileUpload
-                            label='Choose File'
-                            type="file"
-                            id='attachFile'
-                            autoComplete='attachFile'
+                        <SelectField
+                            label='Preferred Response Method'
+                            id='response'
+                            autoComplete='response'
+                            options={responseMethodOpts}
                         />
                     </div>
+                </div>
 
-                    <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
-                        <div>
-                            <SelectField
-                                label='Priority'
-                                id='priority'
-                                autoComplete='priority'
-                                options={priorityOpts}
-                            />
-                        </div>
-                        <div>
-                            <SelectField
-                                label='Preferred Response Method'
-                                id='response'
-                                autoComplete='response'
-                                options={responseMethodOpts}
-                            />
-                        </div>
-                    </div>
-
-                    {/* Submit Button */}
-                    <div className="text-center pt-4">
-                        <SubmitBtn
-                            type="submit"
-                            id='submit'
-                            btnName='Submit Request'
-                        />
-                    </div>
-                    {error && (
-                        <Alert variant="filled" severity="error">
-                            {error}
-                        </Alert>
-                    )}
-                </form>
-            </div>
+                {/* Submit Button */}
+                <div className="text-center pt-4">
+                    <SubmitBtn
+                        type="submit"
+                        id='submit'
+                        btnName='Submit Request'
+                    />
+                </div>
+                {error && (
+                    <Alert variant="filled" severity="error">
+                        {error}
+                    </Alert>
+                )}
+            </form>
+        </div>
     );
 }
