78916ea2a0d09c0b69bcf47ad747277872027a38
Raushan Sinha <raushankrsinha2004@gmail.com>
2026-02-04 10:38:28 +0530

    Created a new file

---
+++ src/pages/UserProfile/Mentorship.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Sidebar from '../../components/Layout/Sidebar';
+
+const Mentorship = () => {
+    return (
+        <>
+            <Sidebar />
+        </>
+    )
+}
+
+export default Mentorship;
