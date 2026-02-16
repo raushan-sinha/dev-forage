c5ff9a15030470dbdd9253ba0b99d1e7c35fb3fe
Raushan Sinha <raushankrsinha2004@gmail.com>
2026-02-16 20:02:04 +0530

    Created a file for settings

---
+++ src/pages/UserProfile/Settings.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Sidebar from '../../components/Layout/Sidebar';
+
+const Settings = () => {
+    return (
+        <>
+            <Sidebar />
+        </>
+    )
+}
+
+export default Settings;
